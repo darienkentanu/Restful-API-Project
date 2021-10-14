daddf56f5f4b30572ef044cd8153cec6763a6e7b
Rizka Khairani <rizkakhairanii@gmail.com>
2021-10-14 14:35:40 +0700

    edit model

--- models/cart.go
+++ models/cart.go
@@ -3,18 +3,19 @@ package models
 import "time"
 
 type Cart struct {
-	ID           int         `gorm:"primarykey;AUTO_INCREMENT" json:"id"`
-	CustomerID	 int		 `json:"customer_id"`
-	Customer     Customer	 `gorm:"foreignkey:CustomerID"`
+	ID           int         `gorm:"primarykey;AUTO_INCREMENT" json:"id" form:"id"`
 	CreatedAt    time.Time
 }
 
 type CartItem struct {
-	ID          int      `gorm:"primarykey;not null;AUTO_INCREMENT" json:"id"`
-	CartID		int		 `json:"cart_id"`
+	ID          int      `gorm:"primarykey;not null;AUTO_INCREMENT" json:"id" form:"id"`
+	CartID		int		 `json:"cart_id" form:"cart_id"`
 	Cart     	Cart	 `gorm:"foreignkey:CartID"`
-	ProductID	int		 `json:"product_id"`
+	ProductID	int		 `json:"product_id" form:"product_id"`
 	Product     Product	 `gorm:"foreignkey:ProductID"`
-	Quantity    int      `gorm:"type:bigint;not null" json:"quantity"`
+	Quantity    int      `gorm:"type:bigint;not null" json:"quantity" form:"quantity"`
+	AddedAt    	time.Time
+	CheckoutID	int		 `json:"checkout_id" form:"checkout_id"`
+	Checkout	Checkout `gorm:"foreignkey:CheckoutID"`
 }
 
--- models/transaction.go
+++ models/transaction.go
@@ -2,20 +2,19 @@ package models
 
 import "time"
 
-type Transaction struct {
-	ID              int            `gorm:"primarykey;AUTO_INCREMENT" json:"id"`
-	CustomerID		int			   `json:"customer_id"`
-	Customer	    Customer       `gorm:"foreignkey:CustomerID"`
-	Amount          int            `gorm:"type:int;not null" json:"amount"`
+type Checkout struct {
+	ID              int          `gorm:"primarykey;AUTO_INCREMENT" json:"id"`
 	CreatedAt       time.Time
 }
 
-type CheckoutItem struct {
-	ID              int          `gorm:"primarykey;AUTO_INCREMENT" json:"id"`
-	TransactionID	int			 `json:"transaction_id"`
-	Transaction		Transaction  `gorm:"foreignkey:TransactionID"`
-	ProductID		int			 `json:"product_id"`
-	Product     	Product	     `gorm:"foreignkey:ProductID"`
-	Quantity        int          `gorm:"type:bigint;not null" json:"quantity"`
+type Transaction struct {
+	ID              int            `gorm:"primarykey;AUTO_INCREMENT" json:"id" form:"id"`
+	CustomerID		int			   `json:"customer_id" form:"customer_id"`
+	Customer	    Customer       `gorm:"foreignkey:CustomerID"`
+	Address         string 		   `gorm:"type:longtext;" json:"address" form:"address"`
+	Courier			string 		   `gorm:"type:varchar(30);not null" json:"courier" form:"courier"`
+	PaymentStatus	int			   `gorm:"type:tinyint;not null;default:0;" json:"payment_status" form:"payment_status"`
 	CreatedAt       time.Time
+	CheckoutID		int			   `gorm:"primarykey;" json:"checkout_id" form:"checkout_id"`
+	Checkout	    Checkout       `gorm:"foreignkey:CheckoutID;"`
 }
