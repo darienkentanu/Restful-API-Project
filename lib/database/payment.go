dc5ff5e87c37261d080a499b1cdd8cb16c0ead05
Darien Kentanu <darienkentanu@gmail.com>
2021-10-17 18:09:45 +0700

    edit payment amount func

--- lib/database/payment.go
+++ lib/database/payment.go
@@ -3,12 +3,16 @@ package database
 import (
 	"altastore/config"
 	"altastore/models"
+	"errors"
 )
 
 func GetPaymentAmount(idTransaction int) (amount int, err error) {
 	var p models.PaymentDetail
 	if err := config.InitDB().Where("transaction_id = ?", idTransaction).Last(&p).Error; err != nil {
 		return 0, err
+	}
+	if p.Amount == 0 {
+		return 0, errors.New("internal server error, -> payment amount couldn't be zero")
 	}
 	return p.Amount, nil
 }
