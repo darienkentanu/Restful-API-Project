99af2d095eb2fa9a3efa127ffec6a442f004d110
Rizka Khairani <rizkakhairanii@gmail.com>
2021-10-17 18:53:01 +0700

    add cart controller

--- lib/database/cart.go
+++ lib/database/cart.go
@@ -12,3 +12,12 @@ func CreateCart(cart models.Cart) error {
 
 	return nil
 }
+
+func GetUserIdInCart(cartID int) (models.Cart, error) {
+	var cart models.Cart
+	if err := config.InitDB().Find(&cart, "id = ?", cartID).Error; err != nil {
+		return cart, err
+	}
+
+	return cart, nil
+}
